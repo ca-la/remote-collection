a3bb017f8f866c9998f44f573c0e727c85f4cf68
Sam Swanke <skswanke@gmail.com>
2018-10-29 10:13:56 -0400

    Add unit tests

---
+++ src/__tests__/concat-resources.ts
@@ -0,0 +1,39 @@
+import test from 'ava';
+import * as RD from '@cala/remote-data';
+import Collection from '../index';
+import { Item, items } from './fixtures';
+
+const moreItems = [{ id: 'c', foo: 'test' }, { id: 'd', foo: 'test' }];
+
+test('with no items loaded, #withList', t => {
+  const col = new Collection<Item>().withList('id', items).concatResources('id', moreItems);
+  t.deepEqual(
+    col.knownIds,
+    RD.success<string[], string[]>(['a', 'b', 'c', 'd']),
+    'sets knownIds to returned ids'
+  );
+  t.deepEqual(
+    col.entities,
+    {
+      a: RD.success<string[], Item>(items[0]),
+      b: RD.success<string[], Item>(items[1]),
+      c: RD.success<string[], Item>(moreItems[0]),
+      d: RD.success<string[], Item>(moreItems[1])
+    },
+    'sets entities to { [id: string]: RemoteSuccess(Item) }'
+  );
+});
+
+test('with item loading failure, #withList', t => {
+  const col = new Collection<Item>().withListFailure('Failed').concatResources('id', items);
+  t.deepEqual(
+    col.knownIds,
+    RD.success<string[], string[]>(['a', 'b']),
+    'sets knownIds to returned ids'
+  );
+  t.deepEqual(
+    col.entities,
+    { a: RD.success<string[], Item>(items[0]), b: RD.success<string[], Item>(items[1]) },
+    'sets entities to { [id: string]: RemoteSuccess(Item) }'
+  );
+});
