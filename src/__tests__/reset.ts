50b348c71a5562c619ec4c037a98a10bee92a8c1
Scott Trinh <scott@scotttrinh.com>
2019-04-10 11:29:56 -0400

    Add tests for "initial" methods: remove, reset

---
+++ src/__tests__/reset.ts
@@ -0,0 +1,51 @@
+import test from 'ava';
+import * as RD from '@cala/remote-data';
+import RemoteCollection from '../index';
+import { Item, items } from './fixtures';
+
+test('with no items loaded, #reset', t => {
+  const col = new RemoteCollection<Item>('id').reset();
+
+  t.deepEqual(col.view(), RD.initial, 'view is set to initial');
+});
+
+test('with some items loaded, #reset', t => {
+  const col = new RemoteCollection<Item>('id').withList(items).reset();
+
+  t.deepEqual(col.view(), RD.initial, 'view is reset to initial');
+});
+
+test('with view failure, #reset', t => {
+  const col = new RemoteCollection<Item>('id').withListFailure(new Error('Oh no')).reset();
+
+  t.deepEqual(col.view(), RD.initial, 'view is reset to initial');
+});
+
+test('with no items loaded at the view key, #reset', t => {
+  const col = new RemoteCollection<Item>('id')
+    .withList(items, 'someOtherViewKey')
+    .reset('someViewKey');
+
+  t.deepEqual(col.view('someViewKey'), RD.initial, 'view is set to initial');
+  t.deepEqual(col.view('someOtherViewKey'), RD.success<string[], Item[]>(items));
+});
+
+test('with some items loaded, #reset', t => {
+  const col = new RemoteCollection<Item>('id')
+    .withList(items, 'someViewKey')
+    .withList(items, 'someOtherViewKey')
+    .reset('someViewKey');
+
+  t.deepEqual(col.view('someViewKey'), RD.initial, 'view is set to initial');
+  t.deepEqual(col.view('someOtherViewKey'), RD.success<string[], Item[]>(items));
+});
+
+test('with view failure, #reset', t => {
+  const col = new RemoteCollection<Item>('id')
+    .withListFailure(new Error('Oh no'), 'someViewKey')
+    .withList(items, 'someOtherViewKey')
+    .reset();
+
+  t.deepEqual(col.view('someViewKey'), RD.initial, 'view is set to initial');
+  t.deepEqual(col.view('someOtherViewKey'), RD.success<string[], Item[]>(items));
+});
