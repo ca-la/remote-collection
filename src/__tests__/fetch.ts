d7c4abe6adc45042ab9eda6d95d96499bddb4b62
Scott Trinh <scott@scotttrinh.com>
2019-04-10 14:59:38 -0400

    Update Pending/Refresh methods: fetch, refresh

--- src/__tests__/fetch.ts
+++ src/__tests__/fetch.ts
@@ -4,26 +4,24 @@ import RemoteCollection from '../index';
 import { Item, items } from './fixtures';
 
 test('with no items loaded, #fetch', t => {
-  const col = new RemoteCollection<Item>().fetch('a');
-  t.deepEqual(col.entities, { a: RD.pending }, 'transitions id to pending');
+  const col = new RemoteCollection<Item>('id').fetch('a');
+
+  t.deepEqual(col.find('a'), RD.pending, 'sets missing resource to pending');
 });
 
 test('with item loading failure, #fetch', t => {
-  const col = new RemoteCollection<Item>()
-    .withListFailure('There was a problem getting the list')
+  const col = new RemoteCollection<Item>('id')
+    .withResourceFailure('a', 'There was a problem getting the list')
     .fetch('a');
-  t.deepEqual(col.entities, { a: RD.pending }, 'transitions id to pending');
+
+  t.deepEqual(col.find('a'), RD.pending, 'sets failed resource to pending');
 });
 
 test('with items loaded, #fetch', t => {
-  const col = new RemoteCollection<Item>().withList('id', items);
-  t.deepEqual(col.entities, {
-    a: RD.success<string[], Item>(items[0]),
-    b: RD.success<string[], Item>(items[1])
-  });
-  const fetched = col.fetch('a');
-  t.deepEqual(fetched.entities, {
-    a: RD.refresh<string[], Item>(items[0]),
-    b: RD.success<string[], Item>(items[1])
-  });
+  const col = new RemoteCollection<Item>('id').withList(items).fetch('a');
+  t.deepEqual(
+    col.find('a'),
+    RD.refresh<string[], Item>(items[0]),
+    'changes an existing success into a refresh'
+  );
 });
