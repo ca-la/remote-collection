1c6e5f653cf0c821826d8ef74c9bc5461093ca8b
Sebastian Coraccio <sebastiancoraccio@gmail.com>
2019-08-19 16:27:58 -0400

    Add strict type annotation

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { fromNullable } from 'fp-ts/lib/Option';
+import { fromNullable, Option } from 'fp-ts/lib/Option';
 import { ById } from './types';
 
-export const safeGet = <A>(byId: ById<A>, id: string) => fromNullable(byId[id]);
+export const safeGet = <A>(byId: ById<A>, id: string): Option<A> => fromNullable(byId[id]);
